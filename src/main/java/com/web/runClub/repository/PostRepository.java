0f08b79efa428431ebb62cab54e63eba968085d8
L1cht <lensdima@gmail.com>
2023-05-08 20:23:56 +0300

    up 0.0.1

---
+++ src/main/java/com/web/runClub/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package com.web.runClub.repository;
+
+import com.web.runClub.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository <Post,Long>{
+
+}
---
+++ src/test/java/com/web/runClub/RunClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.runClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
